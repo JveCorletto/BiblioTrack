8a24f5d7f79cf95f3b056e822c2c7f2bf3be0ce6
André Martínez <est.j5martinez@gmail.com>
2023-08-23 00:14:24 -0600

    Initial commit

---
+++ SysBiblioteca.API/Controllers/CatalogosController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SysBiblioteca.API.Models;
+
+namespace SysBiblioteca.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CatalogosController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetStates()
+        {
+            var allStates = new List<Estados>() { 
+                new Estados
+                {
+                    IdEstado = 1,
+                    Estado = "Activo"
+                }
+            };
+
+            return Ok(allStates);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<Estados>> getAState(int id)
+        {
+            var allStates = new List<Estados>() {
+                new Estados
+                {
+                    IdEstado = 1,
+                    Estado = "Activo"
+                }
+            };
+
+            return Ok(allStates);
+        }
+    }
+}
---
+++ SysBiblioteca.API/Models/Estados.cs
@@ -0,0 +1,8 @@
+﻿namespace SysBiblioteca.API.Models
+{
+    public class Estados
+    {
+        public Int32 IdEstado { get; set; }
+        public String? Estado { get; set; }
+    }
+}
