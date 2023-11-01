8a94339016c5ee66f643bbe71a0e690b192034c9
André Martínez <est.j5martinez@gmail.com>
2023-10-31 21:14:07 -0600

    Se agregó Controlador para vistas de Seguridad en Interfaz

---
+++ SysBiblioteca.UI/Controllers/PersonalInterno/SeguridadController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SysBiblioteca.UI.Controllers.PersonalInterno
+{
+    public class SeguridadController : Controller
+    {
+        public IActionResult Usuarios()
+        {
+            return View();
+        }
+
+        public IActionResult Empleados()
+        {
+            return View();
+        }
+
+        public IActionResult Permisos()
+        {
+            return View();
+        }
+    }
+}
