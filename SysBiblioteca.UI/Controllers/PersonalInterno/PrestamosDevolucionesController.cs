08cd6d207ec02a1f60ae3d23bb30e0ec7ee63632
Diego <diegoayala1232@hotmail.com>
2023-11-12 16:30:44 -0600

    Creacion de la vista y controller Devoluciones para el modulo Prestamos y Devoluciones

---
+++ SysBiblioteca.UI/Controllers/PersonalInterno/PrestamosDevolucionesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SysBiblioteca.UI.Controllers.PersonalInterno
+{
+    public class PrestamosDevolucionesController : Controller
+    {
+        public IActionResult Devoluciones()
+        {
+            return View();
+        }
+    }
+}
