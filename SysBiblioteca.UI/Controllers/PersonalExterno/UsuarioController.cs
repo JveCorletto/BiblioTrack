37a83788df923bde076170cf2d56a05182da0d06
CristabelUrias <wendyastrid9@gmail.com>
2023-11-12 19:04:03 -0600

    se agregó vista para busqueda y reserva

---
+++ SysBiblioteca.UI/Controllers/PersonalExterno/UsuarioController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SysBiblioteca.UI.Controllers.PersonalExterno
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult BusquedaReserva()
+        {
+            return View();
+        }
+    }
+}
